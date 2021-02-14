b3ea3c6c9ef5eb758b1648bdd2af5eeea517e722
Pavel Shramov <psha@nguni.psha.org.ru>
2021-02-14 12:17:16 +0300

    tll-sys: Added missing files for message and scheme bindings

---
+++ tll-sys/src/scheme.rs
@@ -0,0 +1,8 @@
+#![allow(dead_code,
+         non_camel_case_types,
+         non_upper_case_globals,
+         non_snake_case)]
+
+#[ repr ( C ) ]
+#[ derive ( Debug, Copy, Clone ) ]
+pub struct tll_scheme_t { _unused : [ u8; 0 ], }
