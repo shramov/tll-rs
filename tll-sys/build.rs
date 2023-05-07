5512cf81d034521ee81a76ea5c4f82e5aca8776e
Pavel Shramov <psha@nguni.psha.org.ru>
2023-05-07 17:07:23 +0300

    tll-sys: Use pkg-config to find tll

--- tll-sys/build.rs
+++ tll-sys/build.rs
@@ -1,11 +1,5 @@
-//use cc;
+use pkg_config;
 
 fn main() {
-    //println!("rustc-flags=-ltll");
-    //println!("cargo:rustc-link-lib=dylib=tll");
-    println!("cargo:rustc-link-lib=tll");
-    println!("cargo:rustc-link-search=native=/home/psha/src/tll/build/src");
-    //println!("cargo:rerun-if-changed=src/channel/inline.c");
-    //println!("cargo:rustc-link-search=native=/home/psha/src/tll-export/build/lib");
-    //cc::Build::new().flag("-I/home/psha/src/tll/src").file("src/channel/inline.c").compile("tll-channel-static")
+    pkg_config::probe_library("tll").unwrap();
 }
