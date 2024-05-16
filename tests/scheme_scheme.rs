bd961643eb79a8d75f3183636ea8781861c66994
Pavel Shramov <psha@nguni.psha.org.ru>
2024-05-16 21:46:48 +0300

    tests: Test new message bindings
    Fix unalinged accesses

--- tests/scheme_scheme.rs
+++ tests/scheme_scheme.rs
@@ -1,78 +1,124 @@
-#![allow(
-    dead_code,
-    non_camel_case_types,
-    non_upper_case_globals,
-    non_snake_case
-)]
+#![allow(dead_code, non_camel_case_types, non_upper_case_globals, non_snake_case)]
 
+pub use tll::mem::{MemOffset, MemRead};
+pub use tll::scheme::mem::ByteString;
 pub use tll::scheme::*;
 
 pub const SCHEME_STRING : &str = "yamls+gz://eJylkt2KgzAQRu/7FLkLlBXWVor4KssisUnLgEbJz4WUvvtmBN3EyZZd9i5jTs7ky1gwLQbVMG59xw+M3UD10jZhxVjBHutmzd+Ymydcg3Y1fx6K9eBg73gQZMPK9x8NQA0p4CPAZwkoL4kjlEQSIz7PwPmUaEJJNDHi8wxcqkQTSqKJEZ9nbjEjR9/1ijCyPEWPI9UVBtHjtz14jeN3s1NLPU4ORm0b9uC4xXGezoC+8+de0NXpedpCGFPtnu+j+iTY5Mw3dcxHxz9uY7Agub0RePXW2/3EklRG2bH3WGK2Bd6Srg6adbNrS6b0Qq9/qXcwqGkMulaK+S/3D3jcAT3tInrV4z8Z0g5fMvYzTg==";
 
-#[repr(C, packed(1))]
-#[derive(Debug, Clone, Copy)]
-pub struct sub {
-    pub s8: i8,
+#[derive(Debug)]
+pub struct sub<Buf: MemRead> {
+    data: MemOffset<Buf>,
 }
-impl Binder for sub {
-    fn bind(data: &[u8]) -> Option<&Self> {
-        if data.len() < std::mem::size_of::<Self>() {
+impl<Buf: MemRead + Copy> sub<Buf> {
+    pub fn bind(data: Buf) -> Option<Self> {
+        if data.mem_size() < 1 {
             return None;
         }
-        <i8 as Binder>::bind(&data[0..])?; // s8
-        Some(unsafe { bind_unchecked::<Self>(data) })
+        Some(Self {
+            data: MemOffset::new(data),
+        })
     }
-}
 
-#[repr(C, packed(1))]
-pub struct msg {
-    pub i8: i8,
-    pub u8: u8,
-    pub i16: i16,
-    pub u16: u16,
-    pub i32: i32,
-    pub u32: u32,
-    pub i64: i64,
-    pub u64: u64,
-    pub f64: f64,
-    pub d128: tll::decimal128::Decimal128,
-    pub c16: tll::scheme::ByteString<16>,
-    pub b8: [u8; 8],
-    pub arr4: tll::scheme::Array<i32, i8, 4>,
-    pub ptr: tll::scheme::OffsetPtr<i64>,
-    pub sub: sub,
-    pub duration_us: tll::scheme::Duration<i32, tll::scheme::Micro>,
-    pub duration_ns: tll::scheme::Duration<u64, tll::scheme::Nano>,
-    pub timepoint_days: tll::scheme::TimePoint<i32, tll::scheme::RatioDay>,
-    pub timepoint_ns: tll::scheme::TimePoint<u64, tll::scheme::Nano>,
+    pub fn bind_unchecked(data: Buf) -> Self {
+        Self {
+            data: MemOffset::new(data),
+        }
+    }
+
+    pub fn get_s8(&self) -> i8 {
+        self.data.mem_get_primitive::<i8>(0)
+    }
 }
-impl MsgId for msg {
-    const MSGID: i32 = 10;
+#[derive(Debug)]
+pub struct msg<Buf: MemRead = &'static [u8]> {
+    data: MemOffset<Buf>,
 }
-impl Binder for msg {
-    fn bind(data: &[u8]) -> Option<&Self> {
-        if data.len() < std::mem::size_of::<Self>() {
+impl<Buf: MemRead + Copy> msg<Buf> {
+    pub fn bind(data: Buf) -> Option<Self> {
+        if data.mem_size() < 128 {
             return None;
         }
-        <i8 as Binder>::bind(&data[0..])?; // i8
-        <u8 as Binder>::bind(&data[1..])?; // u8
-        <i16 as Binder>::bind(&data[2..])?; // i16
-        <u16 as Binder>::bind(&data[4..])?; // u16
-        <i32 as Binder>::bind(&data[6..])?; // i32
-        <u32 as Binder>::bind(&data[10..])?; // u32
-        <i64 as Binder>::bind(&data[14..])?; // i64
-        <u64 as Binder>::bind(&data[22..])?; // u64
-        <f64 as Binder>::bind(&data[30..])?; // f64
-        <tll::decimal128::Decimal128 as Binder>::bind(&data[38..])?; // d128
-        <tll::scheme::ByteString<16> as Binder>::bind(&data[54..])?; // c16
-        <[u8; 8] as Binder>::bind(&data[70..])?; // b8
-        <tll::scheme::Array<i32, i8, 4> as Binder>::bind(&data[78..])?; // arr4
-        <tll::scheme::OffsetPtr<i64> as Binder>::bind(&data[95..])?; // ptr
-        <sub as Binder>::bind(&data[103..])?; // sub
-        <tll::scheme::Duration<i32, tll::scheme::Micro> as Binder>::bind(&data[104..])?; // duration_us
-        <tll::scheme::Duration<u64, tll::scheme::Nano> as Binder>::bind(&data[108..])?; // duration_ns
-        <tll::scheme::TimePoint<i32, tll::scheme::RatioDay> as Binder>::bind(&data[116..])?; // timepoint_days
-        <tll::scheme::TimePoint<u64, tll::scheme::Nano> as Binder>::bind(&data[120..])?; // timepoint_ns
-        Some(unsafe { bind_unchecked::<Self>(data) })
+        // Array
+        // Pointer
+        if sub::<&[u8]>::bind(&data.as_mem()[103..]).is_none() {
+            return None;
+        }
+        Some(Self {
+            data: MemOffset::new(data),
+        })
     }
+
+    pub fn bind_unchecked(data: Buf) -> Self {
+        Self {
+            data: MemOffset::new(data),
+        }
+    }
+
+    pub fn get_i8(&self) -> i8 {
+        self.data.mem_get_primitive::<i8>(0)
+    }
+    pub fn get_u8(&self) -> u8 {
+        self.data.mem_get_primitive::<u8>(1)
+    }
+    pub fn get_i16(&self) -> i16 {
+        self.data.mem_get_primitive::<i16>(2)
+    }
+    pub fn get_u16(&self) -> u16 {
+        self.data.mem_get_primitive::<u16>(4)
+    }
+    pub fn get_i32(&self) -> i32 {
+        self.data.mem_get_primitive::<i32>(6)
+    }
+    pub fn get_u32(&self) -> u32 {
+        self.data.mem_get_primitive::<u32>(10)
+    }
+    pub fn get_i64(&self) -> i64 {
+        self.data.mem_get_primitive::<i64>(14)
+    }
+    pub fn get_u64(&self) -> u64 {
+        self.data.mem_get_primitive::<u64>(22)
+    }
+    pub fn get_f64(&self) -> f64 {
+        self.data.mem_get_primitive::<f64>(30)
+    }
+    pub fn get_d128(&self) -> tll::decimal128::Decimal128 {
+        self.data.mem_get_primitive::<tll::decimal128::Decimal128>(38)
+    }
+    pub fn get_c16(&self) -> tll::scheme::mem::ByteString<16, MemOffset<Buf>> {
+        tll::scheme::mem::ByteString::<16, MemOffset<Buf>>::new(self.data.view(54))
+    }
+    pub fn get_b8(&self) -> &[u8] {
+        &self.data.as_mem()[70..70 + 8]
+    }
+    pub fn get_arr4(&self) -> tll::scheme::mem::Array<u8, i32, 4, MemOffset<Buf>> {
+        tll::scheme::mem::Array::<u8, i32, 4, MemOffset<Buf>>::new(self.data.view(78))
+    }
+    pub fn get_ptr(&self) -> Option<tll::scheme::mem::OffsetPtr<i64, tll::scheme::mem::OffsetPtrDefault, Buf>> {
+        tll::scheme::mem::OffsetPtr::<i64, tll::scheme::mem::OffsetPtrDefault, Buf>::new(self.data.view(95))
+    }
+    pub fn get_sub(&self) -> sub<Buf> {
+        sub::<Buf> {
+            data: self.data.view(103),
+        }
+    }
+    pub fn get_duration_us(&self) -> tll::scheme::Duration<i32, tll::scheme::Micro> {
+        self.data
+            .mem_get_primitive::<tll::scheme::Duration<i32, tll::scheme::Micro>>(104)
+    }
+    pub fn get_duration_ns(&self) -> tll::scheme::Duration<u64, tll::scheme::Nano> {
+        self.data
+            .mem_get_primitive::<tll::scheme::Duration<u64, tll::scheme::Nano>>(108)
+    }
+    pub fn get_timepoint_days(&self) -> tll::scheme::TimePoint<i32, tll::scheme::RatioDay> {
+        self.data
+            .mem_get_primitive::<tll::scheme::TimePoint<i32, tll::scheme::RatioDay>>(116)
+    }
+    pub fn get_timepoint_ns(&self) -> tll::scheme::TimePoint<u64, tll::scheme::Nano> {
+        self.data
+            .mem_get_primitive::<tll::scheme::TimePoint<u64, tll::scheme::Nano>>(120)
+    }
+}
+impl<Buf: MemRead> MsgId for msg<Buf> {
+    const MSGID: i32 = 10;
 }
