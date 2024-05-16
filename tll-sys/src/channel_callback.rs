fbf27309a5950ee95a0a76dc4e9da09fcb03df08
Pavel Shramov <psha@nguni.psha.org.ru>
2024-05-16 21:42:52 +0300

    tll-sys: Callback structures can be unaligned

--- tll-sys/src/channel_callback.rs
+++ tll-sys/src/channel_callback.rs
@@ -1,25 +1,37 @@
 use crate::channel::*;
 
-pub fn channel_callback_data( internal : &tll_channel_internal_t, msg : *const tll_msg_t ) -> i32
-{
-        let list = unsafe { std::slice::from_raw_parts(internal.data_cb as * const tll_channel_callback_pair_t, internal.data_cb_size as usize) };
-        for cb in list {
-		unsafe { cb.cb.unwrap()(internal.self_, msg, cb.user) };
+pub fn channel_callback_data(internal: &tll_channel_internal_t, msg: *const tll_msg_t) -> i32 {
+    for i in 0..internal.data_cb_size {
+        unsafe {
+            let ptr = internal.data_cb.offset(i as isize);
+            let cb = std::ptr::addr_of!((*ptr).cb).read_unaligned();
+            let user = std::ptr::addr_of!((*ptr).user).read_unaligned();
+            cb.unwrap()(internal.self_, msg, user);
         }
-	0
+    }
+    0
 }
 
-pub fn channel_callback( internal : &tll_channel_internal_t, msg : *const tll_msg_t ) -> i32
-{
-        if msg.is_null() { return 0 }
-        let t = unsafe { (*msg).type_ } as u32;
-        if t as tll_msg_type_t == TLL_MESSAGE_DATA { return channel_callback_data(internal, msg) };
+pub fn channel_callback(internal: &tll_channel_internal_t, msg: *const tll_msg_t) -> i32 {
+    if msg.is_null() {
+        return 0;
+    }
+    let t = unsafe { (*msg).type_ } as u32;
+    if t as tll_msg_type_t == TLL_MESSAGE_DATA {
+        return channel_callback_data(internal, msg);
+    };
 
-        let list = unsafe { std::slice::from_raw_parts(internal.cb as * const tll_channel_callback_pair_t, internal.cb_size as usize) };
-        for cb in list {
-            if cb.mask & (1u32 << t) != 0 {
-                unsafe { cb.cb.unwrap()(internal.self_, msg, cb.user) };
+    for i in 0..internal.data_cb_size {
+        unsafe {
+            let ptr = internal.data_cb.offset(i as isize);
+            let mask = std::ptr::addr_of!((*ptr).mask).read_unaligned();
+            if mask & (1u32 << t) != 0 {
+                continue;
             }
+            let cb = std::ptr::addr_of!((*ptr).cb).read_unaligned();
+            let user = std::ptr::addr_of!((*ptr).user).read_unaligned();
+            cb.unwrap()(internal.self_, msg, user);
         }
-	0
+    }
+    0
 }
